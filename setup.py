5b58d32de32964d9149fc0907701cd14177f0933
Benjamin Motz <benjamin.motz@mailbox.org>
2019-03-09 11:44:35 +0100

    Add basic setup.py script for easy installation
    Still misses some important meta-information

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='icsv2ledger',
+    version='0.1.0',
+    description="This is a command-line utility to convert CSV files of transactions, such as you might download from an online banking service, into the format used by John Wiegley's excellent Ledger system.",
+    author='Quentin Stafford-Fraser',
+    author_email='quentin@pobox.com',
+    url="http://github.com/quentinsf/icsv2ledger",
+    python_requires='>=3.2',
+    entry_points={
+        'console_scripts': [
+            'icsv2ledger = icsv2ledger:main'
+        ]
+    },
+)
